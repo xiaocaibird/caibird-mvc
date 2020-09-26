4b96ac05425bce30fa7bee79f47a44270a610e30
cmZhou <332877152@qq.com>
2020-09-26 22:16:31 +0800

    v1.3.0 feat:1.去掉GetJssProps

--- src/web/@types/declare/dReact.d.ts
+++ src/web/@types/declare/dReact.d.ts
@@ -6,10 +6,6 @@ declare namespace dReact {
     type State = never;
     type Props = { children?: React.ReactNode };
 
-    type GetJssProps<T extends import('react-jss').Styles> = {
-        classes?: Partial<Record<keyof T, string>>;
-    };
-
     type GetProps<TComponent extends React.Component | React.ComponentType<any> | React.FunctionComponent<any>> =
         TComponent extends React.Component<infer Props> ? Props :
         TComponent extends React.ComponentType<infer Props> ? Props :
