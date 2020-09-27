0d7a2127c12212cf2b50aa44565ac9715e408b40
cmZhou <332877152@qq.com>
2020-09-27 20:43:34 +0800

    v1.3.2

--- src/web/helper/hReact.tsx
+++ src/web/helper/hReact.tsx
@@ -10,33 +10,49 @@ export abstract class HReact<TRootContext> {
     }) { }
     public readonly rootContext = createContext(this.options.defaultContext);
 
-    public readonly getAsyncComponent = (importComponent?: dp.PromiseFunc<any[], { default: React.ComponentType<any> }>) =>
-        class AsyncComponent extends React.Component<object, { component: null | React.ComponentType<any> }> {
-            constructor(props: object) {
+    public readonly withAsync = <T extends object>(importComponent?: dp.PromiseFunc<any[], { default: React.ComponentType<T> }>) => {
+        const createHocDisplayName = this.createHocDisplayName;
+
+        const With = class extends React.Component<T, { Component: null | React.ComponentType<T> }> {
+            private static _displayName = 'withAsync';
+            public static get displayName() {
+                return With._displayName;
+            }
+
+            constructor(props: T) {
                 super(props);
 
                 this.state = {
-                    component: null
+                    Component: null
                 };
             }
 
             public render() {
-                const Component = this.state.component;
+                const { Component } = this.state;
 
                 return Component ? <Component {...this.props} /> : null;
             }
 
             public async componentDidMount() {
                 if (importComponent) {
-                    const { default: component } = await importComponent();
+                    const { default: Component } = await importComponent();
+
+                    With._displayName = createHocDisplayName('withAsync', Component);
 
                     this.setState({
-                        component
+                        Component
                     });
                 }
             }
-        }
-    public readonly getAutoDestroyComponent = <TProps extends { visible?: boolean }>(Component: React.ComponentType<TProps>) => memo(
+        };
+
+        return With;
+    }
+
+    public readonly withAutoDestroy = <TProps extends { visible?: boolean }>(Component: React.ComponentType<TProps>) => memo(
         (props: TProps) => props.visible ? <Component {...props} /> : null
     )
+
+    public readonly createHocDisplayName = (hocName: string, WrappedComponent: React.ComponentType<any>) =>
+        `${hocName}(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`
 }
