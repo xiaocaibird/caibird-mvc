698555dd0ce1e908907fbb6a53af1f08a7c5a206
cmZhou <332877152@qq.com>
2020-10-05 23:27:52 +0800

    v1.3.4

--- src/web/helper/hReact.tsx
+++ src/web/helper/hReact.tsx
@@ -2,7 +2,9 @@
  * @Creater cmZhou
  * @Desc react helper
  */
-import React, { createContext, memo } from 'react';
+import React, { createContext, createRef, memo } from 'react';
+
+import { uFunction } from '../util/uFunction';
 
 export abstract class HReact<TRootContext> {
     constructor(protected readonly options: {
@@ -13,7 +15,7 @@ export abstract class HReact<TRootContext> {
     public readonly withAsync = <T extends React.ComponentType<any>>(importComponent?: dp.PromiseFunc<any[], { default: T }>, displayName?: string) => {
         const createHocDisplayName = this.createHocDisplayName;
 
-        const With = class extends React.Component<dReact.GetProps<T>, { Component?: T }> {
+        const With = class extends React.PureComponent<dReact.GetProps<T> & { onAsyncInnerDidMount?(): void }, { Component?: T; isClassComponent?: boolean }> {
             public static displayName = displayName || createHocDisplayName('withAsync');
 
             constructor(props: dReact.GetProps<T>) {
@@ -23,19 +25,37 @@ export abstract class HReact<TRootContext> {
                 };
             }
 
-            public render() {
-                const { Component } = this.state;
+            private isCallOnInnerDidMount = false;
 
-                return Component ? <Component {...this.props as any} /> : null;
+            public readonly innerRef = createRef<React.ReactInstance>();
+
+            public render() {
+                const { Component, isClassComponent } = this.state;
+
+                return Component ?
+                    isClassComponent ? <Component ref={this.innerRef} {...this.props as any} /> :
+                        <Component {...this.props as any} /> : null;
             }
 
             public async componentDidMount() {
                 if (importComponent) {
                     const { default: Component } = await importComponent();
 
+                    const isClassComponent = uFunction.checkExtendsClass(Component, React.Component);
+
                     this.setState({
-                        Component
+                        Component,
+                        isClassComponent
                     });
+                }
+            }
+
+            public componentDidUpdate() {
+                const { onAsyncInnerDidMount } = this.props;
+                const { Component } = this.state;
+                if (Component && !this.isCallOnInnerDidMount) {
+                    onAsyncInnerDidMount && onAsyncInnerDidMount();
+                    this.isCallOnInnerDidMount = true;
                 }
             }
         };
