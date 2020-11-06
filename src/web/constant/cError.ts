01d9a0e78bdf5cabdff0abe767b4d695ebcd8e43
cmZhou <332877152@qq.com>
2020-11-06 17:36:50 +0800

    feat:1.cerror

--- src/web/constant/cError.ts
+++ src/web/constant/cError.ts
@@ -7,39 +7,57 @@ let onAppError: undefined | ((err: Error) => dp.PromiseOrSelf<void>);
 export const setOnAppError = (fn: (err: Error) => dp.PromiseOrSelf<void>) => onAppError = fn;
 
 window.addEventListener('error', evt => {
-    let error = evt.error as Error | undefined;
-    if (!error) {
-        try {
-            const split = evt.message.split(':');
-            const errName = split[0].trim() as keyof typeof cError;
-            const errJson = JSON.parse(split[1].trim());
-            const ErrClass = cError[errName];
-            error = new (ErrClass as any)() as Error;
+    try {
+        let error = evt.error as Error | undefined;
+        if (!error) {
+            let errName: keyof typeof cError | '' = '';
+            try {
+                const index = evt.message.indexOf(':');
 
-            Object.assign(error, errJson);
-        } catch {
-            error = new Error();
-            error.message = evt.message;
-            error.stack = `${evt.filename} | lineno: ${evt.lineno} | colno: ${evt.colno}`;
+                errName = evt.message.slice(0, index).trim() as keyof typeof cError;
+                const jsonStr = evt.message.slice(index + 1);
+
+                const errJson = JSON.parse(jsonStr);
+                const ErrClass = cError[errName];
+
+                error = new (ErrClass as any)() as Error;
+                Object.assign(error, errJson);
+            } catch {
+                try {
+                    // tslint:disable-next-line: no-unsafe-any
+                    error = new (window as any)[errName]() as Error;
+                } catch {
+                    error = new Error();
+                }
+                error.message = evt.message;
+                error.stack = `${evt.filename} | lineno: ${evt.lineno} | colno: ${evt.colno}`;
+                error.name = errName || 'Error';
+            }
         }
+        onAppError && onAppError(error);
+    } catch (e) {
+        console.error('onerror fail', e);
     }
-    onAppError && onAppError(error);
 });
 
 window.addEventListener('unhandledrejection', evt => {
-    onAppError && onAppError(evt.reason as Error);
+    try {
+        evt.promise.catch(err => onAppError && onAppError(err as Error));
+    } catch (e) {
+        console.error('onunhandledrejection fail', e);
+    }
 });
 
 namespace _cError {
     export class CommonError extends Error {
         constructor(
             public readonly options: dError.Options,
-            public readonly logOptions: dReport.ErrorLogOptions | false = false
+            public readonly logOptions: dReport.ErrorLogOptions | false = false,
+            public readonly name = CommonError.name
         ) {
             super();
             this.message = JSON.stringify(this);
         }
-        public readonly name = CommonError.name;
     }
 
     export class ApiFetchFail extends CommonError {
@@ -51,9 +69,8 @@ namespace _cError {
             public readonly options: dError.Options,
             public readonly logOptions: dReport.ErrorLogOptions | false = false
         ) {
-            super(options, logOptions);
+            super(options, logOptions, ApiFetchFail.name);
         }
-        public readonly name = ApiFetchFail.name;
     }
 
     export class ApiJsonResultEmpty extends CommonError {
@@ -64,9 +81,8 @@ namespace _cError {
             public readonly options: dError.Options,
             public readonly logOptions: dReport.ErrorLogOptions | false = false
         ) {
-            super(options, logOptions);
+            super(options, logOptions, ApiJsonResultEmpty.name);
         }
-        public readonly name = ApiJsonResultEmpty.name;
     }
 
     export class ApiJsonResultError extends CommonError {
@@ -78,9 +94,8 @@ namespace _cError {
             public readonly options: dError.Options,
             public readonly logOptions: dReport.ErrorLogOptions | false = false
         ) {
-            super(options, logOptions);
+            super(options, logOptions, ApiJsonResultError.name);
         }
-        public readonly name = ApiJsonResultError.name;
     }
 
     export class LoginError extends CommonError {
@@ -92,9 +107,8 @@ namespace _cError {
             public readonly options: dError.Options,
             public readonly logOptions: dReport.ErrorLogOptions | false = false
         ) {
-            super(options, logOptions);
+            super(options, logOptions, LoginError.name);
         }
-        public readonly name = LoginError.name;
     }
 
     export class VersionMismatch extends CommonError {
@@ -102,9 +116,8 @@ namespace _cError {
             public readonly options: dError.Options,
             public readonly logOptions: dReport.ErrorLogOptions | false = false
         ) {
-            super(options, logOptions);
+            super(options, logOptions, VersionMismatch.name);
         }
-        public readonly name = VersionMismatch.name;
     }
 
     export class Noop extends Error {
