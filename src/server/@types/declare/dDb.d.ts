7513767eb12c59f7faf91e6911ad5e5b69b527b5
cmZhou <332877152@qq.com>
2021-01-12 10:21:17 +0800

    feat:1.调整

---
+++ src/server/@types/declare/dDb.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @Creater cmZhou
+ * @Desc db 常用类型
+ */
+declare namespace dDb {
+    namespace S {
+        type Config = {
+            username: string;
+            password: string;
+            host: string;
+            port: number;
+        };
+    }
+}
---
+++ src/server/@types/enum/eDb.d.ts
@@ -0,0 +1,52 @@
+/**
+ * @Creater cmZhou
+ * @Desc db常用枚举
+ */
+declare namespace eDb {
+    namespace S {
+        const enum OrderType {
+            ASC = 'ASC',
+            DESC = 'DESC'
+        }
+
+        const enum Op {
+            eq = 'eq',
+            ne = 'ne',
+            gte = 'gte',
+            gt = 'gt',
+            lte = 'lte',
+            lt = 'lt',
+            not = 'not',
+            is = 'is',
+            in = 'in',
+            notIn = 'notIn',
+            like = 'like',
+            notLike = 'notLike',
+            iLike = 'iLike',
+            notILike = 'notILike',
+            regexp = 'regexp',
+            notRegexp = 'notRegexp',
+            iRegexp = 'iRegexp',
+            notIRegexp = 'notIRegexp',
+            between = 'between',
+            notBetween = 'notBetween',
+            overlap = 'overlap',
+            contains = 'contains',
+            contained = 'contained',
+            adjacent = 'adjacent',
+            strictLeft = 'strictLeft',
+            strictRight = 'strictRight',
+            noExtendRight = 'noExtendRight',
+            noExtendLeft = 'noExtendLeft',
+            and = 'and',
+            or = 'or',
+            any = 'any',
+            all = 'all',
+            values = 'values',
+            col = 'col',
+            placeholder = 'placeholder',
+            join = 'join',
+            raw = 'raw'
+        }
+    }
+}
