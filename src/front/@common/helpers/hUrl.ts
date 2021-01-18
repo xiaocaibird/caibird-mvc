a904fb0c687c3538b2ba76f8899e307e23c1ccb8
cmZhou <332877152@qq.com>
2021-01-19 01:55:32 +0800

    v2.1.1

---
+++ src/front/@common/helpers/hUrl.ts
@@ -0,0 +1,14 @@
+/**
+ * @Creater cmZhou
+ * @Desc url helper
+ */
+export abstract class HUrl {
+    constructor(protected readonly options: {
+        prefix?: string;
+        imgPath: string;
+    }) { }
+
+    public readonly full = (url = '') => `${location.protocol}//${location.host}${this.options.prefix || ''}${url}`;
+
+    public readonly image = (imgName: string) => `${this.options.imgPath}/${imgName}`;
+}
---
+++ src/front/web/helpers/hUrl.ts
@@ -0,0 +1,9 @@
+/**
+ * @Creater cmZhou
+ * @Desc url helper
+ */
+import { HUrl as base } from '../../@common/helpers/hUrl';
+
+export abstract class HUrl extends base {
+
+}
