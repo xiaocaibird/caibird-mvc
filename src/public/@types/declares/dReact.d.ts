05715c1d6d25b48b1e37894d97207bddb3c36944
cmZhou <332877152@qq.com>
2021-03-03 23:03:42 +0800

    feat:1.@typescript-eslint/no-shadow

--- src/public/@types/declares/dReact.d.ts
+++ src/public/@types/declares/dReact.d.ts
@@ -7,12 +7,12 @@ declare namespace dReact {
     type Props = { children?: React.ReactNode };
 
     type GetProps<TComponent extends React.Component | React.ComponentType | React.FunctionComponent> =
-        TComponent extends React.Component<infer Props> ? Props :
-        TComponent extends React.ComponentType<infer Props> ? Props :
-        TComponent extends React.FunctionComponent<infer Props> ? Props :
-        TComponent extends React.MemoExoticComponent<React.FunctionComponent<infer Props>> ? Props : never;
+        TComponent extends React.Component<infer P> ? P :
+        TComponent extends React.ComponentType<infer P> ? P :
+        TComponent extends React.FunctionComponent<infer P> ? P :
+        TComponent extends React.MemoExoticComponent<React.FunctionComponent<infer P>> ? P : never;
 
     type GetState<TComponent extends React.Component | React.ComponentClass> =
         // eslint-disable-next-line @typescript-eslint/ban-types
-        TComponent extends React.Component<{}, infer State> ? State : TComponent extends React.ComponentClass<{}, infer State> ? State : never;
+        TComponent extends React.Component<{}, infer S> ? S : TComponent extends React.ComponentClass<{}, infer S> ? S : never;
 }
