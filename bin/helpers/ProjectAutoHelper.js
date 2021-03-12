08ed79c654ea8f290bd95323294793e842885483
cmzhou <332877152@qq.com>
2021-03-12 23:10:16 +0800

    feat:1.迁移

--- bin/helpers/ProjectAutoHelper.js
+++ bin/helpers/ProjectAutoHelper.js
@@ -57,6 +57,142 @@ class ProjectAutoHelper {
     };
 
     getRunEnv = () => runEnvArgs[this.getEnv()];
+
+    build = () => {
+        const projectName = getProjectName();
+        const env = getEnv();
+
+        const rsp = exec(`npm run dist ${projectName} ${env} && npm run webpack ${projectName}`);
+        process.exit(rsp.code);
+    };
+
+    checkTsc = () => {
+        const projectName = getProjectName();
+
+        const rsp = exec(`npm run eslint ${projectName} && npm run tsc ${projectName}`);
+        process.exit(rsp.code);
+    };
+
+    createDbEntity = () => {
+        const projectName = getProjectName();
+
+        if (hasServerProjectNames.includes(projectName)) {
+            createDbEntityHelper(`src/${projectName}/server/models`);
+        } else {
+            printf(`Error: the project 【${projectName}】 no has server`, ColorsEnum.RED);
+            process.exit(1);
+        }
+    };
+
+    dist = () => {
+        const projectName = getProjectName();
+        const runEnv = getRunEnv();
+
+        const rsp = exec(`npm run clear:bundle && npm run check-tsc ${projectName} && cross-env RUN_ENV=${runEnv} PROJECT_NAME=${projectName} npm run gulp ${projectName}`);
+        process.exit(rsp.code);
+    };
+
+    eslint = () => {
+        const projectName = getProjectName();
+        const unionProjectNames = getUnionProjectNames(projectName);
+
+        // TODO ./src/serverEntry.ts
+        const rsp = exec(`eslint -c ./src/${projectName}/.eslintrc.json --cache --cache-location \"./.eslint/${projectName}-cache\" -f codeframe --ext .ts,.tsx,.js,.jsx ./src/${projectName} ${unionProjectNames.map(item => `./src/${item}`).join(' ')} ./src/@common ${process.argv.includes('fix') ? ' --fix' : ''}`);
+        process.exit(rsp.code);
+    };
+
+    gulp = () => {
+        const projectName = getProjectName();
+
+        if (hasGulpProjectNames.includes(projectName)) {
+            const rsp = exec(`npm run clear:dist && node node_modules/gulp/bin/gulp.js dist --gulpfile .tsc/src/${projectName}/build/gulp/gulpfile.js`);
+            process.exit(rsp.code);
+        } else {
+            printf(`Error: the project 【${projectName}】 no has gulpfile`, ColorsEnum.RED);
+            process.exit(1);
+        }
+    };
+
+    release = () => {
+        const projectName = getProjectName();
+
+        if (allowReleaseProjectNames.includes(projectName)) {
+            const region = 'shenzhen';
+
+            releaseHelper({
+                baseCommitId: '6604a8a67f6e144aa47dd730a28ece26604ecd23',
+                ossConfig: {
+                    region: `oss-cn-${region}`,
+                    bucket: 'zhitu-api',
+                    dir: 'operations-sys/jsbundle', // TODO,
+                    jsBundleDir: path.join(process.cwd(), 'assets/bundle/js'),
+                    getOssOptions: () => {
+                        const secret = require(
+                            path.relative(__dirname,
+                                path.join(process.cwd(), `/dist/${projectName}/server/_dev/setting/secret/global`))).default;
+
+                        const accessKeyId = secret.publicOssConfig.accessKeyId;
+                        const accessKeySecret = secret.publicOssConfig.accessKeySecret;
+
+                        return {
+                            accessKeyId,
+                            accessKeySecret,
+                        };
+                    },
+                },
+            });
+        } else {
+            printf(`Error: the project 【${projectName}】 no allow release`, ColorsEnum.RED);
+            process.exit(1);
+        }
+    };
+
+    start = () => {
+        const projectName = getProjectName();
+        const isReal = process.argv[3] === 'real-debug';
+
+        if (allowStartProjectNames.includes(projectName)) {
+            if (taroProjectNames.includes(projectName)) {
+                const rsp = exec(`npm run check-tsc ${projectName} && cross-env NODE_ENV=${isReal ? 'production' : 'development'} node bin/taro build --type weapp --watch ${projectName}`);
+                process.exit(rsp.code);
+            } else {
+                const rsp = exec(`npm run kill-port && npm run dist ${projectName} local && node app`);
+                process.exit(rsp.code);
+            }
+        } else {
+            printf(`Error: the project 【${projectName}】 no allow start`, ColorsEnum.RED);
+            process.exit(1);
+        }
+    };
+
+    taro = () => {
+        const { printPkgVersion } = require('@tarojs/cli/dist/util');
+        const Cli = require('@tarojs/cli/dist/cli').default;
+
+        printPkgVersion();
+
+        const cli = new Cli(path.join(process.cwd(), `src/${process.argv[process.argv.length - 1]}/front/taro`));
+        cli.run();
+    };
+
+    tsc = () => {
+        const projectName = getProjectName();
+
+        const rsp = exec(`npm run clear:tsc && node node_modules/typescript/bin/tsc -p src/${projectName}/tsconfig.json`);
+        process.exit(rsp.code);
+    };
+
+    webpack = () => {
+        const projectName = getProjectName();
+
+        if (hasWebpackProjectNames.includes(projectName)) {
+            const rsp = exec(`npm run clear:bundle && node node_modules/webpack/bin/webpack.js --config dist/${projectName}/build/webpack/webpack.config`);
+            process.exit(rsp.code);
+        } else {
+            printf(`Error: the project 【${projectName}】 no has webpack`, ColorsEnum.RED);
+            process.exit(1);
+        }
+    };
 }
 
 module.exports = ProjectAutoHelper;
