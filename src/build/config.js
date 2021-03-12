5f2aa49067e6210f0198fd92a71910ef3e3bb14f
cmzhou <332877152@qq.com>
2021-03-12 19:25:11 +0800

    feat:1.调整

--- src/build/config.js
+++ src/build/config.js
@@ -2,6 +2,38 @@
  * @Owners cmZhou
  * @Title build config
  */
+// TODO 类型限制
+const runEnvArgs = {
+    production: 'production',
+    exp: 'expProduction',
+
+    test: 'test',
+    dev: 'devTest',
+    local: 'localTest',
+};
+
+const envValues = {
+    NODE_ENV: {
+        PRODUCTION: 'production',
+        DEVELOPMENT: 'development',
+    },
+    RUN_ENV: {
+        PRODUCTION: runEnvArgs.production,
+        EXP_PRODUCTION: runEnvArgs.exp,
+
+        TEST: runEnvArgs.test,
+        DEV_TEST: runEnvArgs.dev,
+        LOCAL_TEST: runEnvArgs.local,
+    },
+};
+
+const isExpProduction = process.env.RUN_ENV === envValues.RUN_ENV.EXP_PRODUCTION; // 仅体验环境
+const isProduction = process.env.RUN_ENV === envValues.RUN_ENV.PRODUCTION || isExpProduction; // 正式环境或体验环境
+
+const isLocalTest = !process.env.RUN_ENV || process.env.RUN_ENV === envValues.RUN_ENV.LOCAL_TEST; // 仅本地调试
+const isDevTest = process.env.RUN_ENV === envValues.RUN_ENV.DEV_TEST; // 仅开发环境
+const isTest = process.env.RUN_ENV === envValues.RUN_ENV.TEST || isDevTest || isLocalTest; // 本地调试或开发环境或测试环境
+
 module.exports = {
     envs: {
         production: 'production',
@@ -11,14 +43,7 @@ module.exports = {
         dev: 'dev',
         local: 'local',
     },
-    runEnvArgs: {
-        production: 'production',
-        exp: 'expProduction',
-
-        test: 'test',
-        dev: 'devTest',
-        local: 'localTest',
-    },
+    runEnvArgs,
     envTitles: {
         production: '【生产】环境',
         exp: '【体验】环境',
@@ -34,5 +59,16 @@ module.exports = {
         test: 'test',
         dev: 'dev',
         local: '',
+    },
+
+    envValues,
+
+    runStatus: {
+        isProduction,
+        isExpProduction,
+
+        isTest,
+        isDevTest,
+        isLocalTest,
     },
 };
--- src/build/ini/index.ts
+++ src/build/ini/index.ts
@@ -2,31 +2,14 @@
  * @Owners cmZhou
  * @Title 项目构建初始化文件
  */
-import { runEnvArgs } from '../config';
-namespace ini {
-    export const envValues = {
-        NODE_ENV: {
-            PRODUCTION: 'production',
-            DEVELOPMENT: 'development',
-        },
-        RUN_ENV: {
-            PRODUCTION: runEnvArgs.production,
-            EXP_PRODUCTION: runEnvArgs.exp,
-
-            TEST: runEnvArgs.test,
-            DEV_TEST: runEnvArgs.dev,
-            LOCAL_TEST: runEnvArgs.local,
-        },
-    } as const;
-
-    export const isExpProduction = process.env.RUN_ENV === envValues.RUN_ENV.EXP_PRODUCTION; // 仅体验环境
-    export const isProduction = process.env.RUN_ENV === envValues.RUN_ENV.PRODUCTION || isExpProduction; // 正式环境或体验环境
-
-    export const isLocalTest = !process.env.RUN_ENV || process.env.RUN_ENV === envValues.RUN_ENV.LOCAL_TEST; // 仅本地调试
-    export const isDevTest = process.env.RUN_ENV === envValues.RUN_ENV.DEV_TEST; // 仅开发环境
-    export const isTest = process.env.RUN_ENV === envValues.RUN_ENV.TEST || isDevTest || isLocalTest; // 本地调试或开发环境或测试环境
-
-    export const NODE_ENV_VALUE = isProduction ? envValues.NODE_ENV.PRODUCTION : envValues.NODE_ENV.DEVELOPMENT;
+import { envValues, runStatus } from '../config';
+namespace _ini {
+    export const NODE_ENV_VALUE = runStatus.isProduction ? envValues.NODE_ENV.PRODUCTION : envValues.NODE_ENV.DEVELOPMENT;
 }
 
-export default ini as dp.DeepReadonly<typeof ini>;
+const ini: dp.DeepReadonly<typeof _ini & typeof runStatus> = {
+    ...runStatus,
+    ..._ini,
+};
+
+export default ini;
