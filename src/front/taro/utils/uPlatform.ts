1c5317fa5e36178d42a76071b6ecd6ae323825b1
cmzhou <332877152@qq.com>
2021-03-21 10:16:15 +0800

    feat:1. uPlatfirn

--- src/front/taro/utils/uPlatform.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @Owners cmZhou
- * @Title platform 工具
- */
-import base from '../../@com/utils/uPlatform';
-
-export const uPlatform = base;
-export default uPlatform;
--- src/front/web/utils/uPlatform.ts
+++ src/front/web/utils/uPlatform.ts
@@ -2,7 +2,42 @@
  * @Owners cmZhou
  * @Title platform 工具
  */
-import base from '../../@com/utils/uPlatform';
+import compareVersions, { CompareOperator } from 'compare-versions';
 
-export const uPlatform = base;
+import { cPlatform } from '../consts/cPlatform';
+
+export namespace uPlatform {
+    const OS_NAME_LC = cPlatform.OS_NAME.toLowerCase();
+
+    export const checkOS = (OSName: ePlatform.F.OsName) => OS_NAME_LC === OSName.toLowerCase();
+
+    export const compareOSVersion = (OSName: ePlatform.F.OsName, compareVersion: string) => {
+        const isOS = checkOS(OSName);
+
+        if (!isOS) return false;
+
+        const str = compareVersion.trim();
+
+        let compare = '';
+        let version = '';
+
+        if (str.startsWith('<') || str.startsWith('>') || str.startsWith('=')) {
+            compare += str[0];
+            if ((str.startsWith('<') || str.startsWith('>')) && str[1] === '=') {
+                compare += str[1];
+                version = str.slice(2);
+            } else {
+                version = str.slice(1);
+            }
+        } else {
+            compare = '=';
+            version = str.slice(1);
+        }
+
+        const OSVersion = cPlatform.OS_VERSION.replace('NT ', '');
+
+        return compareVersions.compare(OSVersion, version, compare as CompareOperator);
+    };
+}
+
 export default uPlatform;
