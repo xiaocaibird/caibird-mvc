27e3b8b5653391d0989f107b95f34b07e3bd1699
zhengzehao <zzh@miyuan.com>
2021-03-30 18:54:27 +0800

    fix safeStringify

--- src/public/utils/uObject/safeStringify.ts
+++ src/public/utils/uObject/safeStringify.ts
@@ -3,7 +3,7 @@
  * @Title public stringify
  * @Details 避免循环引用抛出异常 https://github.com/moll/json-stringify-safe
  */
-import safeStringify from 'json-stringify-safe';
+import stringify from 'json-stringify-safe';
 
-export const stringify = safeStringify;
-export default stringify;
+export const safeStringify = stringify;
+export default safeStringify;
