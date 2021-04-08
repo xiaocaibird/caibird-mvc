8f587f4d6905d694fb15572e0f0c97e0e47ffab1
xiaocaibird <332877152@qq.com>
2021-04-08 23:21:31 +0800

    feat:1.del dSetting

--- src/server/app/helpers/settingHelper.ts
+++ src/server/app/helpers/settingHelper.ts
@@ -5,6 +5,52 @@
 import fs from 'fs';
 import path from 'path';
 
+export declare namespace SettingDeclare {
+    type CheckType<T extends dCaibird.Obj<dCaibird.Obj | string | undefined>> = T;
+
+    type GlobalConfig = CheckType<{
+    }>;
+
+    type GlobalSecret = CheckType<{
+        tracingAnalysisEndpoint?: string,
+
+        publicDbConfig?: {
+            username: string,
+            password: string,
+            host: string,
+            port: number,
+        },
+        publicRedisConfig?: {
+            host: string,
+            password: string,
+            port: number,
+        },
+
+        publicOssConfig?: {
+            accessKeyId: string,
+            accessKeySecret: string,
+        },
+    }>;
+
+    type CheckInterface<T extends dCaibird.Obj | string | undefined> = T;
+
+    type CheckCustomConfig = CheckInterface<CustomConfig[keyof CustomConfig]>;
+    interface CustomConfig {
+        version?: string,
+
+        reportConfig?: {
+            pathWhiteList?: string[],
+            pathIgnoreList?: string[],
+            dbLogPathWhiteListWhenAlways?: string[],
+            whiteListCtxKeys?: string[],
+            whiteListCtxValues?: string[],
+        },
+    }
+
+    type CheckCustomSecret = CheckInterface<CustomSecret[keyof CustomSecret]>;
+    interface CustomSecret { }
+}
+
 class SettingHelper {
     public static readonly instance = new SettingHelper();
     private constructor() { }
@@ -21,7 +67,7 @@ class SettingHelper {
 
     public readonly setProjectName = (name: string) => this.projectName = name;
 
-    public readonly getValue = <T extends dSetting.S.CustomConfig | dSetting.S.CustomSecret | dSetting.S.GlobalConfig | dSetting.S.GlobalSecret, TKey extends keyof T>(
+    public readonly getValue = <T extends SettingDeclare.CustomConfig | SettingDeclare.CustomSecret | SettingDeclare.GlobalConfig | SettingDeclare.GlobalSecret, TKey extends keyof T>(
         key: TKey, filename: string, dft?: T[TKey],
     ): dCaibird.DeepPartial<T[TKey]> | undefined => {
         try {
@@ -48,13 +94,13 @@ class SettingHelper {
         }
     };
 
-    public readonly getGlobalConfig = <TKey extends keyof dSetting.S.GlobalConfig>(key: TKey, dft?: dSetting.S.GlobalConfig[TKey]) => this.getValue<dSetting.S.GlobalConfig, TKey>(key, this.GLOBAL_CONFIG_NAME, dft);
+    public readonly getGlobalConfig = <TKey extends keyof SettingDeclare.GlobalConfig>(key: TKey, dft?: SettingDeclare.GlobalConfig[TKey]) => this.getValue<SettingDeclare.GlobalConfig, TKey>(key, this.GLOBAL_CONFIG_NAME, dft);
 
-    public readonly getGlobalSecret = <TKey extends keyof dSetting.S.GlobalSecret>(key: TKey, dft?: dSetting.S.GlobalSecret[TKey]) => this.getValue<dSetting.S.GlobalSecret, TKey>(key, this.GLOBAL_SECRET_NAME, dft);
+    public readonly getGlobalSecret = <TKey extends keyof SettingDeclare.GlobalSecret>(key: TKey, dft?: SettingDeclare.GlobalSecret[TKey]) => this.getValue<SettingDeclare.GlobalSecret, TKey>(key, this.GLOBAL_SECRET_NAME, dft);
 
-    public readonly getCustomConfig = <TKey extends keyof dSetting.S.CustomConfig>(key: TKey, dft?: dSetting.S.CustomConfig[TKey]) => this.getValue<dSetting.S.CustomConfig, TKey>(key, this.CUSTOM_CONFIG_NAME, dft);
+    public readonly getCustomConfig = <TKey extends keyof SettingDeclare.CustomConfig>(key: TKey, dft?: SettingDeclare.CustomConfig[TKey]) => this.getValue<SettingDeclare.CustomConfig, TKey>(key, this.CUSTOM_CONFIG_NAME, dft);
 
-    public readonly getCustomSecret = <TKey extends keyof dSetting.S.CustomSecret>(key: TKey, dft?: dSetting.S.CustomSecret[TKey]) => this.getValue<dSetting.S.CustomSecret, TKey>(key, this.CUSTOM_SECRET_NAME, dft);
+    public readonly getCustomSecret = <TKey extends keyof SettingDeclare.CustomSecret>(key: TKey, dft?: SettingDeclare.CustomSecret[TKey]) => this.getValue<SettingDeclare.CustomSecret, TKey>(key, this.CUSTOM_SECRET_NAME, dft);
 }
 
 export const settingHelper = SettingHelper.instance;
