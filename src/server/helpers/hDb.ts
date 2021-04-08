8542b99633f6a5b8e032ed56bd9d2ab1b6d17dc3
xiaocaibird <332877152@qq.com>
2021-04-09 01:25:25 +0800

    feat:1.fix

--- src/server/helpers/hDb.ts
+++ src/server/helpers/hDb.ts
@@ -17,6 +17,48 @@ export declare namespace DbDeclare {
     };
 }
 
+export declare namespace DbEnum {
+    const enum Op {
+        eq = 'eq',
+        ne = 'ne',
+        gte = 'gte',
+        gt = 'gt',
+        lte = 'lte',
+        lt = 'lt',
+        not = 'not',
+        is = 'is',
+        in = 'in',
+        notIn = 'notIn',
+        like = 'like',
+        notLike = 'notLike',
+        iLike = 'iLike',
+        notILike = 'notILike',
+        regexp = 'regexp',
+        notRegexp = 'notRegexp',
+        iRegexp = 'iRegexp',
+        notIRegexp = 'notIRegexp',
+        between = 'between',
+        notBetween = 'notBetween',
+        overlap = 'overlap',
+        contains = 'contains',
+        contained = 'contained',
+        adjacent = 'adjacent',
+        strictLeft = 'strictLeft',
+        strictRight = 'strictRight',
+        noExtendRight = 'noExtendRight',
+        noExtendLeft = 'noExtendLeft',
+        and = 'and',
+        or = 'or',
+        any = 'any',
+        all = 'all',
+        values = 'values',
+        col = 'col',
+        placeholder = 'placeholder',
+        join = 'join',
+        raw = 'raw',
+    }
+}
+
 export abstract class HDb {
     protected readonly baseOptions: Sequelize.Options = {
         pool: {
