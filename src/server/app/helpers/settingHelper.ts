c6b1650b08f2456729df6e48eb6b0ad3566b612e
cmZhou <332877152@qq.com>
2021-04-11 15:52:55 +0800

    feat

--- src/server/app/helpers/settingHelper.ts
+++ src/server/app/helpers/settingHelper.ts
@@ -72,7 +72,7 @@ class SettingHelper {
     ): Caibird.dp.DeepPartial<T[TKey]> | undefined => {
         try {
             if (Caibird.env.IS_LOCAL_TEST) {
-                const relativePath = path.relative(__dirname, path.join(process.cwd(), `/dist/${this.projectName}/server/_dev/setting/${filename}`)).replace(/\\/g, '/');
+                const relativePath = path.relative(__dirname, path.join(process.cwd(), `/dist/${this.projectName}/server/helpers/hSetting/_dev/${filename}`)).replace(/\\/g, '/');
                 // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-require-imports
                 const obj = (require(relativePath) as { default: T }).default;
                 return (obj[key] || dft) as Caibird.dp.DeepPartial<T[TKey]>;
