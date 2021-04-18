04fb85fb28f5f39fb699c0c21655767de7341290
cmZhou <332877152@qq.com>
2021-04-19 01:44:29 +0800

    fix

--- bin/updatePackage.js
+++ bin/updatePackage.js
@@ -7,5 +7,5 @@ const updatePackageHelper = require('./helpers/updatePackageHelper');
 updatePackageHelper({
     packageJson: require('../package.json'),
     depLockList: [],
-    devDepLockList: ['tapable', 'sequelize', 'webpack', 'webpack-cli', 'html-webpack-plugin', 'terser-webpack-plugin', '@types/terser-webpack-plugin', 'history', '@types/history'],
+    devDepLockList: ['tapable', 'cssnano', 'sequelize', 'webpack', 'webpack-cli', 'html-webpack-plugin', 'terser-webpack-plugin', '@types/terser-webpack-plugin', 'history', '@types/history'],
 });
