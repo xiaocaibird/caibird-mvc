983e80ef7882c3a2edf62533908ca5f43b717599
cmZhou <332877152@qq.com>
2021-04-19 01:14:02 +0800

    v2.6.6

--- src/build/webpack/webpack.config.ts
+++ src/build/webpack/webpack.config.ts
@@ -235,7 +235,8 @@ export default (webpackOptions: WebpackOptions, webpackConfig: webpack.Configura
                     openAnalyzer: true,
                     analyzerMode: 'static',
                     statsFilename: '../.log/BundleAnalyzerStats.json',
-                }),
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                }) as any,
             );
         }
 
