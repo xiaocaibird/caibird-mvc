7d618e8d0a52f5d4ec7c83a67f9d74b750d0980a
xiaocaibird <332877152@qq.com>
2021-04-23 10:38:58 +0800

    hApp

--- src/server/helpers/hApp.ts
+++ src/server/helpers/hApp.ts
@@ -3,16 +3,11 @@
  * @Title app helper
  */
 import { dApp } from '../@types/declares';
-import App from '../app';
 
 export abstract class HApp<TApp extends dApp.App> {
     protected constructor(public readonly app: TApp) { }
 
-    public readonly ctx = this.app.helpers.context;
+    public readonly ctx: TApp['helpers']['context'] = this.app.helpers.context;
 
-    public readonly mvc = this.app.helpers.mvc;
-
-    public readonly report = App.staticHelpers.report;
-
-    public readonly setting = App.staticHelpers.setting;
+    public readonly mvc: TApp['helpers']['mvc'] = this.app.helpers.mvc;
 }
