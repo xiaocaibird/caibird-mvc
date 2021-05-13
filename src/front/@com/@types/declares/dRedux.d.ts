523c6b610768cf8787fe20b5790ac8f1759f4942
xiaocaibird <332877152@qq.com>
2021-05-13 16:37:46 +0800

    fix

--- src/front/@com/@types/declares/dRedux.d.ts
+++ src/front/@com/@types/declares/dRedux.d.ts
@@ -8,6 +8,8 @@ export namespace dRedux {
         [K in string]: (() => { type: K }) | ((payload?: any) => { type: K, payload: any })
     };
 
+    type BaseState = Caibird.dp.Obj<Caibird.dp.Obj>;
+
     type StandardActions<TActions extends BaseActions> = {
         [K in keyof TActions]:
         TActions[K] extends (payload?: infer Value) => unknown ?
@@ -22,13 +24,13 @@ export namespace dRedux {
         { type: K }
     };
 
-    type Reducers<TActions, TRequiredActions, TState> = {
+    type Reducers<TActions extends BaseActions, TRequiredActions extends BaseActions, TState extends BaseState> = {
         [K in keyof TState]: {
             defaultState: TState[K],
             handlers: ReducerHandlers<TActions, TRequiredActions, TState[K]>,
         }
     };
-    type ReducerHandlers<TActions, TRequiredActions, TStatePart> = {
+    type ReducerHandlers<TActions extends BaseActions, TRequiredActions extends BaseActions, TStatePart extends Caibird.dp.Obj> = {
         [K in keyof TActions]?: ReducerHandler<TActions, TStatePart, K>;
     } & {
         [K in keyof TRequiredActions]: ReducerHandler<TRequiredActions, TStatePart, K>;
@@ -38,5 +40,5 @@ export namespace dRedux {
 type ActionReturn<TActions extends dRedux.BaseActions> = {
     [K in keyof TActions]: ReturnType<TActions[K]>;
 };
-type ReducerHandler<TActions, S, K extends keyof TActions> = GetActionValue<TActions, K> extends never | undefined ? (state: S) => S : (state: S, payload: GetActionValue<TActions, K>) => S;
-type GetActionValue<TActions, K extends keyof TActions> = TActions[K] extends (payload: infer V) => unknown ? V : TActions[K] extends () => unknown ? never : never;
+type ReducerHandler<TActions extends dRedux.BaseActions, S extends Caibird.dp.Obj, K extends keyof TActions> = GetActionValue<TActions, K> extends never | undefined ? (state: S) => S : (state: S, payload: GetActionValue<TActions, K>) => S;
+type GetActionValue<TActions extends dRedux.BaseActions, K extends keyof TActions> = TActions[K] extends (payload: infer V) => unknown ? V : TActions[K] extends () => unknown ? never : never;
